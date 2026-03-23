fac6917a227cdd6b3d2b4a1487f10fb379ff7a96
meriemccown <255442697+catnapfeverdream@users.noreply.github.com>
2026-03-23 16:24:03 -0400

    create function and write logic for playRound

--- rps.js
+++ rps.js
@@ -1,17 +1,40 @@
-function getComputerChoice(){
+function getComputerChoice(computerChoice){
     var availableMoves = ['rock','paper','scissors'],
         randomNumber = Math.floor(Math.random() * availableMoves.length),
-        opponentMove = availableMoves[randomNumber];
+        computerChoice = availableMoves[randomNumber];
     
-    return opponentMove;
+    return computerChoice;
 }
 
-function getPlayerChoice(){
-    const playerChoice = prompt('make your move!')
-    return playerChoice
+function getPlayerChoice(playerChoice){
+    const playerChoice = prompt('make your move!');
+    playerChoice = playerChoice.tolowercase();
+    return playerChoice;
 }
 
 let humanScore = 0
 
 let computerScore = 0
 
+function playRound(playerChoice,computerChoice){
+    if (playerChoice === 'rock' && computerChoice === 'paper') {
+        computerScore = ++1
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        humanScore = ++1
+    } else if (playerChoice === 'rock' && computerChoice === 'rock') {
+
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        humanScore = ++1
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        computerScore = ++1
+    } else if (playerChoice === 'paper' && computerChoice === 'paper') {
+
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore = ++1
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore = ++1
+    } else if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+        
+    }
+    
+}
